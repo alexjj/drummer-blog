236524b57226ab52a2a6ffaafcbbb01224667420
Alex Johnstone <alexjj@gmail.com>
2021-10-20 15:33:04 +0100

    pull first

--- backup.sh
+++ backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /data/computer/cloud/drummer-blog
+git pull
 curl -O http://drummer.scripting.com/thealexjj/blog.opml
 curl -O http://drummer.scripting.com/thealexjj/about.opml
 curl -O http://drummer.scripting.com/thealexjj/glossary.opml
