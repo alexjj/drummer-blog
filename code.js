a8cba29bdeac0cf7a405084711b71efc6275ef85
Alex Johnstone <alexjj@gmail.com>
2021-10-28 21:44:59 +0100

    update to 0.4.10

--- code.js
+++ code.js
@@ -103,11 +103,17 @@ var urlTwitterServer = "http://electricserver.scripting.com/";
 			}
 		setTabContent (""); //wipe out the blog html before user sees it
 		
-		if (aboutOutline !== undefined) { //10/18/21 by DW
-			console.log ("viewAboutTab: using the pre-built aboutOutline.");
-			processOutlineStruct (aboutOutline)
+		var flBuilt = false;
+		try {
+			if (aboutOutline !== undefined) { //10/18/21 by DW
+				console.log ("viewAboutTab: using the pre-built aboutOutline.");
+				processOutlineStruct (aboutOutline)
+				flBuilt = true;
+				}
 			}
-		else {
+		catch (err) {
+			}
+		if (!flBuilt) {
 			readHttpFileThruProxy (opmlHead.urlAboutOpml, undefined, function (opmltext) {
 				if (opmltext !== undefined) {
 					var theOutline = opml.parse (opmltext);
@@ -785,6 +791,10 @@ function viewLastUpdateString () {
 		$("#idLastBlogUpdate").html ("Updated: " + whenstring + ".");
 		}
 	}
+function viewFooterLastUpdateString () { //10/28/21 by DW
+	var theString = formatDate (new Date (config.now), "%A, %B %e, %Y at %l:%M %p");
+	$("#idLastUpdateStringInFooter").text (theString);
+	}
 function updateSnarkySlogan () { //1/23/19 by DW
 	$("#idSnarkySlogan").html (getRandomSnarkySlogan ());
 	}
@@ -868,6 +878,7 @@ function movePageDownForOldArchivePages () { //9/21/19 by DW
 		$(".divPageBody").css ("margin-top", "270px")
 		}
 	}
+
 function startup () {
 	console.log ("startup");
 	$("#idVersionNumber").text (myVersion);
@@ -881,6 +892,7 @@ function startup () {
 	
 	startTabsIfHomePage (tabParam, function () {
 		viewLastUpdateString (); //9/28/17 by DW
+		viewFooterLastUpdateString (); //10/28/21 by DW
 		updateSnarkySlogan (); //1/23/19 by DW
 		setupJavaScriptFeatures ();
 		setPageTopImageFromMetadata (); //5/4/20 by DW
